a32d4dfc93a1f7c4a36eff911980baeecf3a502a
Orion Follett <orionfollett@gmail.com>
2021-03-04 16:25:51 -0500

    M-Integer and helper functions

--- interpretor.rkt
+++ interpretor.rkt
@@ -8,6 +8,42 @@
 
 ;Abstractions
 
+;get-operation accepts a list in prefix notation
+;Extracts the operation from the expression
+;Ex: (get-operation '(+ 1 2)) -> +
+(define get-operation
+  (lambda (expression)
+    (car expression)
+    ))
+
+;get-first-operand accepts a list in prefix notation
+;Extracts the first operand from the expression
+;Ex: (get-operation '(/ 1 2)) -> 1
+(define get-first-operand
+  (lambda (expression)
+    (car (cdr expression))
+    ))
+
+
+;get-second-operand accepts a list in prefix notation
+;Extracts the first operand from the expression
+;Ex: (get-operation '(/ 1 2)) -> 2
+(define get-second-operand
+  (lambda (expression)
+    (car (cdr (cdr expression)))))
+    
+;M-Integer
+;takes form '(number operation number) possible operations:  + | - | * | / | %
+
+(define M_integer
+  (lambda (expression)
+    (cond
+      [(number? expression) expression]
+      [(eq? (get-operation expression) '+) (+ (M_integer(get-first-operand expression)) (M_integer(get-second-operand expression)))]
+      [(eq? (get-operation expression) '-) (- (M_integer(get-first-operand expression)) (M_integer(get-second-operand expression)))]
+      [(eq? (get-operation expression) '*) (* (M_integer(get-first-operand expression)) (M_integer(get-second-operand expression)))]
+      [(eq? (get-operation expression) '/) (quotient (M_integer(get-first-operand expression)) (M_integer(get-second-operand expression)))]
+      [(eq? (get-operation expression) '%) (remainder (M_integer(get-first-operand expression)) (M_integer(get-second-operand expression)))])))
 
 
 ;Main Interpreter function
@@ -20,4 +56,4 @@
     (parser filename)))
 
 ;Quick Run:
-(interpret "testProgram.txt")
+;(interpret "testProgram.txt")
