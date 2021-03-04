e50d95dc1778530208c769240dd3f870d808ab1f
Orion Follett <orionfollett@gmail.com>
2021-03-04 16:41:28 -0500

    comments

--- interpretor.rkt
+++ interpretor.rkt
@@ -8,6 +8,7 @@
 
 ;Abstractions
 
+;HELPER FUNCTIONS FOR M_INTEGER
 ;get-operation accepts a list in prefix notation
 ;Extracts the operation from the expression
 ;Ex: (get-operation '(+ 1 2)) -> +
@@ -32,8 +33,9 @@
   (lambda (expression)
     (car (cdr (cdr expression)))))
     
-;M-Integer
-;takes form '(number operation number) possible operations:  + | - | * | / | %
+;M-Integer: evaluates integer expressions
+;takes form '(operation number number) possible operations:  + | - | * | / | %
+;EX: (M_integer '(+ (- 1 2) (* 4 5))) -> 19
 
 (define M_integer
   (lambda (expression)
@@ -46,6 +48,24 @@
       [(eq? (get-operation expression) '%) (remainder (M_integer(get-first-operand expression)) (M_integer(get-second-operand expression)))])))
 
 
+
+;parser filename gives a list where each sublist is a statement
+;there are five different types of statements
+
+;variable declaration	(var variable) or (var variable value)
+;assignment	(= variable expression)
+;return	(return expression)
+;if statement	(if conditional then-statement optional-else-statement)
+;while statement	(while conditional body-statement)
+
+;interpret will step through each statement, execute what needs to get done based on that line, and then interpret the rest of the code
+
+;There may need to be an M-state list that is passed nearly everywhere, it will have all variable bindings so '((x 1) (y 3) ...)
+
+;return statement indicates end of the program
+
+;interpretor should probably loop using continuation passing style rather than regular recursion of the cdr of the list
+
 ;Main Interpreter function
 
 ;Accepts a filename ex: (interpret "testProgram.txt")
@@ -53,7 +73,12 @@
 
 (define interpret
   (lambda (filename)
-    (parser filename)))
+    
+    (parser filename)
+
+    
+
+    ))
 
 ;Quick Run:
 ;(interpret "testProgram.txt")
